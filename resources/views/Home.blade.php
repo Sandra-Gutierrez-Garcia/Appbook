1126345dd37c95a3d206916b803adfab5d0e6185
Sandra <lacereza7@gmail.com>
2025-08-29 00:01:34 +0200

    style: Update Popular Books section title style

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -43,5 +43,21 @@
             </div>
         </div>
     </div>
+    <!-- book popular -->
+     <div class="container mx-auto px-4 py-12">
+        <h2 class="text-xl font-semibold text-center text-gray-700 mb-8 uppercase tracking-wide">Popular Books</h2>
+        <div class="items-center justify-center flex flex-wrap gap-8  p-4 rounded-lg">
+            <div class="flex flex-col items-center">
+                <div class="w-40 h-56 bg-violet-200 rounded-lg shadow-lg flex items-center justify-center">
+                </div>
+                <span class="mt-2 text-gray-600">Book Title</span>
+                <span class="text-gray-500 text-sm">description book</span>
+                <span class="text-gray-500 text-sm">Author Name</span>
+                <a href="#" class="text-violet-600 hover:underline">Read More</a>
+            </div>
+
+        </div>
+     </div>
+        
 </body>
 </html>
