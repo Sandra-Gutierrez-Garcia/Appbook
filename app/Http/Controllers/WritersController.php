663040cc667deed41c410019287350408d4d7c88
Sandra <lacereza7@gmail.com>
2026-01-21 16:11:42 +0100

    CONTROLLER WRITER

--- app/Http/Controllers/WritersController.php
+++ app/Http/Controllers/WritersController.php
@@ -4,50 +4,82 @@ namespace App\Http\Controllers;
 
 use App\Models\Writer;
 use Illuminate\Http\Request;
+use App\Http\Requests\WriterRequest;
 
 class WritersController extends Controller
 {
     public function index()
     {
-        return Writers::all();
+
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
+        return view('writers.create');
+    }
+
+    public function store(WriterRequest $request)
+    {
+        try {
+
+            $writer = Writer::create($request->validated());
+            return redirect("/writers/{$writer->id}")->with('success', 'Writer created successfully.');
+        } catch (\Exception $e) {
+            return redirect()->back()->withErrors(['error'
+             => 'Failed to create writer.']);
+        }
+    }
+
+   
+    public function show($id)
+    {
+        $writer = Writer::findOrFail($id);
+        return view('writers.show', compact('writer'));
+    }
+
+    public function edit($id)
+    {
+        $writer = Writer::findOrFail($id);
+        return view('writers.edit', compact('writer'));
+    }
+
+   
+    public function update(WriterRequest $request, $id)
+    {
+        $writer = Writer::find($id);
         
-    }
+        if ($writer != null){
 
-    public function store(Request $request)
-    {
-        //
-    }
+            try {
+                $writer->update($request->validated());
+                return redirect("/writers/{$writer->id}")->with('success', 'Writer updated successfully.');
+            } catch (\Exception $e) {
+                return redirect()->back()->withErrors(['error' 
+                => 'Failed to update writer.']);
+            }
 
-   
-    public function show()
-    {
-        //
-    }
-
-    public function edit()
-    {
-        //
-    }
-
-   
-    public function update(Request $request)
-    {
-        //
+        } else {
+            return redirect()->back()->withErrors(['error' 
+            => 'Writer not found.']);
+        }
+        
     }
 
  
    
-    public function destroy()
+    public function destroy($id)
     {
-        //
+        if($writer = Writer::find($id)){
+            try{
+                $writer->delete();
+                return redirect('/writers')->with('success','writer deleted successfully');
+            } catch(\Exception $e){
+                return redirect()->back()->withErrors(['error'
+                => 'error deleting writer']);
+            }
+        } else {
+            return redirect()->back()->withErrors(['error' 
+            => 'Writer not found.']);
+        }
     }
 }
