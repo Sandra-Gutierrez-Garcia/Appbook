182560b119c5ffa9caba12812ff3d1a0c7f47de2
Sandra <lacereza@gmail.com>
2026-04-12 15:22:25 +0200

    Fix squeezed book cards

--- resources/views/components/book-cart.blade.php
+++ resources/views/components/book-cart.blade.php
@@ -1,4 +1,4 @@
-<div class="flex w-[18%] flex-col items-center border border-purple-300 rounded-lg p-6"> 
+<div class="flex max-w-[220px] min-w-[220px] flex-col items-center border border-purple-300 rounded-lg p-6"> 
                 <div class="w-40 h-56 bg-violet-100 rounded-lg shadow-lg flex items-center justify-center"></div> 
                 <h2 class="mt-6 flex h-16 w-full border-b border-purple-300 text-center text-[14px] font-semibold leading-tight">{{ $book->title }}</h2>
                 <p class="mt-2 w-full text-sm text-gray-600">{{ Str::limit($book->description, 40) }}</p>
